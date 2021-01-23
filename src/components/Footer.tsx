3b554cc638bb52d8983ceb8eb08b40438d072691
Murat Erkin Çiçek <murerkinn@gmail.com>
2021-01-23 17:22:03 +0300

    Create Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='py-3 mt-2 text-center mx-auto'>
+      <h6>Made with 💖 by Murat Erkin Cicek</h6>
+    </footer>
+  )
+}
+
+export default Footer
